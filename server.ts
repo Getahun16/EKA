f79473c87f828dc6bc616a6074fa35fac9694760
Getahun16 <getahunnegash12@gmail.com>
2025-07-19 17:14:34 +0300

    chore: add comment for custom Next.js configuration in server.ts

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import next from "next";
 
 const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
-
+// If you want to use a custom Next.js configuration, you can import it here
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
